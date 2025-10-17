65ac99f480d6b1bedab0e8bbb4dea34d15c82094
Stefan Zieger <stefan.zieger@bom.gov.au>
2025-10-17 16:39:53 +1100

    Typo corrected.

--- cmake/FindMETIS.cmake
+++ cmake/FindMETIS.cmake
@@ -9,7 +9,7 @@ set_target_properties(METIS::METIS PROPERTIES
 
 find_package(GKLIB)
 if(NOT GKLIB_FOUND)
-  message(STATUS "Warning: GKLIB not found (skill -lGKlib")
+  message(STATUS "Warning, GKLIB not found (skip linker flag -lGKlib")
   target_link_libraries(METIS::METIS)
 else()
   target_link_libraries(METIS::METIS INTERFACE GKLIB::GKLIB)
